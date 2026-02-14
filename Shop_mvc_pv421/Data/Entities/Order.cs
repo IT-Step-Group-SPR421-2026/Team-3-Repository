dfc39ceb4adf4df3f9964a37adb291959e2e84c3
axneo27 <astrofymchuk@gmail.com>
2026-02-14 13:20:38 +0200

    Initial commit (clean history)

---
+++ Shop_mvc_pv421/Data/Entities/Order.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Shop_mvc_pv421.Data.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalPrice { get; set; }
+        public string UserId { get; set; } = null!;
+        public User? User { get; set; }
+
+        public ICollection<OrderDetails>? Items { get; set; }
+    }
+}
---
+++ Shop_mvc_pv421/Interfaces/IFileService.cs
@@ -0,0 +1,9 @@
+﻿namespace Shop_mvc_pv421.Interfaces
+{
+    public interface IFileService
+    {
+        Task<string> SaveImage(IFormFile file);
+        Task DeleteImage(string path);
+        Task DeleteProductImageExcept(string?[] exeptFiles);
+    }
+}
